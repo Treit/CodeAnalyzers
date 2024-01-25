b866536fc7382c506e7835bd0b16831c43d6b823
Mike Treit <mtreit@ntdev.microsoft.com>
2024-01-24 18:12:28 -0800

    The code analyzer tutorial - writing my first analyzer.

---
+++ TestApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 123;
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
